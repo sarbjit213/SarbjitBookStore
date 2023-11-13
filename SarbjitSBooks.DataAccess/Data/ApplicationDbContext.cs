de1208736bd8a7f555d476c954ed27a2d9ff4406
W0802242 <W0802242@myscc.ca>
2023-11-13 18:10:17 -0500

    COMPLETE PART 2.1

--- SarbjitSBooks.DataAccess/Data/ApplicationDbContext.cs
+++ SarbjitSBooks.DataAccess/Data/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using SarbjitsBooks.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,5 +14,6 @@ namespace SarbjitBookStore.DataAccess.Data
             : base(options)
         {
         }
+        public DbSet<Category> Categories { get; set; }
     }
 }
