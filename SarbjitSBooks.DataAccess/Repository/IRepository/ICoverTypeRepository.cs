c0d742a721868c1206191c968d758cce9ed93c14
W0802242 <W0802242@myscc.ca>
2023-11-21 12:20:52 -0500

    Add ICoverTypeRepo

---
+++ SarbjitSBooks.DataAccess/Repository/IRepository/ICoverTypeRepository.cs
@@ -0,0 +1,13 @@
+﻿using SarbjitsBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace SarbjitSBooks.DataAccess.Repository.IRepository
+{
+    public interface ICoverTypeRepository : IRepository<CoverType>
+    {
+        void Update(CoverType covertype);
+    }
+}
