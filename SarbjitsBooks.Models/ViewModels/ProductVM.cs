66dab5dc4795a137431a5f75565e3e5057f8edd0
W0802242 <W0802242@myscc.ca>
2023-11-21 13:39:13 -0500

    add ProductVM

---
+++ SarbjitsBooks.Models/ViewModels/ProductVM.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace SarbjitsBooks.Models.ViewModels
+{
+    public class ProductVM
+    {
+        public Product product { get; set; }
+        public Product Product { get; set; }
+        public IEnumerable<SelectListItem> CategoryList { get; set; }
+        public IEnumerable<SelectListItem> CoverTypeList { get; set; }
+
+
+    }
+}
