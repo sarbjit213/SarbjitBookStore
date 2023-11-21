0dfdcb1dad8b4f63b232f60ad4abe8379e5c2aa4
W0802242 <W0802242@myscc.ca>
2023-11-21 12:42:02 -0500

    CoverTypeController Added

---
+++ SarbjitBookStore/Areas/Admin/Controllers/CoverTypeController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SarbjitsBooks.Models;
+using SarbjitSBooks.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SarbjitBookStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CoverTypeController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CoverTypeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            CoverType covertype = new CoverType();
+            if (id == null)
+            {
+                return View(covertype);
+            }
+
+            covertype = _unitOfWork.CoverType.Get(id.GetValueOrDefault());
+            if (covertype == null)
+            {
+                return NotFound();
+            }
+            return View(covertype);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Upsert(CoverType covertype)
+        {
+            if (ModelState.IsValid)
+            {
+                if (covertype.Id == 0)
+                {
+                    _unitOfWork.CoverType.Add(covertype);
+
+                }
+                else
+                {
+                    _unitOfWork.CoverType.Update(covertype);
+                }
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(covertype);
+        }
+
+        //api calls
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var allObj = _unitOfWork.CoverType.GetAll();
+            return Json(new { data = allObj });
+        }
+        #endregion
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.CoverType.Get(id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+            _unitOfWork.CoverType.Remove(objFromDb);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Delete successful" });
+        }
+    }
+}
