69c50e56d07947e0e3f8f904819034dd4193d68c
W0802242 <W0802242@myscc.ca>
2023-11-28 13:19:54 -0500

    add the CoverTypeId in product.cs

--- SarbjitsBooks.Models/Product.cs
+++ SarbjitsBooks.Models/Product.cs
@@ -33,7 +33,7 @@ namespace SarbjitsBooks.Models
 
         [Required]
         public int CoverTypeId { get; set; }
-        [ForeignKey("CategoryId")]
+        [ForeignKey(" CoverTypeId")]
         public CoverType CoverType { get; set; }
 
     }
